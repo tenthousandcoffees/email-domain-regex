1eb409f65d33cde478b25e38ff61b78f964eaabf
Elliott Garcea <elliott@tenthousandcoffees.com>
2016-07-19 11:20:49 -0400

    Add tests for basic email domain matching behaviour.

---
+++ email-domain-regexp.js
@@ -0,0 +1,18 @@
+(function(global, factory) {
+    'use strict';
+    if (typeof module === 'object' && module.exports) {
+        module.exports = factory(
+            typeof _ !== 'undefined' ? _ : require('lodash'),
+            typeof escapeStringRegexp !== 'undefined' ? escapeStringRegexp : require('escape-string-regexp')
+        );
+    } else {
+        global.emailDomainRegexp = factory(_, escapeStringRegexp);
+    }
+})(this, function (_, escapeString) {
+    return function emailDomainRegex(approvedDomains) {
+        var checkDomainRegex = _(approvedDomains)
+            .map(function(domain) { return escapeString(_.trim(domain, ' @')).replace('\\*\\.', '(\\w*\\.|)'); })
+            .join('|');
+        return new RegExp('@(' + checkDomainRegex + ')\\s*$');
+    }
+});
---
+++ test/email-domain-regexp.test.js
@@ -0,0 +1,38 @@
+const should = require('chai').should();
+
+let emailDomainRegexp;
+
+describe('email-domain-regexp', () => {
+
+    beforeEach(() => {
+        emailDomainRegexp = require('../email-domain-regexp');
+    });
+
+    it('should match naked domains.', () => {
+        const wildCardDomain = ['*.test.com'];
+        const subDomains = ['test1@test.com', 'test2@test.com'];
+        const genRegex = emailDomainRegexp(wildCardDomain);
+        subDomains.forEach(subDomain => genRegex.test(subDomain).should.be.true);
+    });
+
+    it('should match subdomains.', () => {
+        const wildCardDomain = ['*.test.com'];
+        const subDomains = ['test@a.test.com', 'test@ab.test.com'];
+        const genRegex = emailDomainRegexp(wildCardDomain);
+        subDomains.forEach(subDomain => genRegex.test(subDomain).should.be.true);
+    });
+
+    it('should match multiple different naked domains.', () => {
+        const wildCardDomains = ['*.domainOne.com', '*.domainTwo.com'];
+        const subDomains = ['test@a.domainOne.com', 'test@ab.domainTwo.com'];
+        const genRegex = emailDomainRegexp(wildCardDomains);
+        subDomains.forEach(subDomain => genRegex.test(subDomain).should.be.true);
+    });
+
+    it('should ignore whitespace in email addresses', () => {
+        const wildCardDomains = ['*.domainOne.com', '*.domainTwo.com'];
+        const subDomains = ['test@a.domainOne.com   ', 'test@ab.domainTwo.com    '];
+        const genRegex = emailDomainRegexp(wildCardDomains);
+        subDomains.forEach(subDomain => genRegex.test(subDomain).should.be.true);
+    })
+});
